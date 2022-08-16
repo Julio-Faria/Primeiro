9350efd29e1b5c1b02c108d5dd997263cc11134b
Julio Faria <juliofariadasilva@hotmail.com>
2022-08-16 11:47:26 -0300

    Adequação e comentário

--- Primeiro/Program.cs
+++ Primeiro/Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program
+﻿using System; // referencia para o name space
+
+namespace PrimeiroProjeto
 {
-    private static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] AssemblyLoadEventArgs) //começo de execução do programa
+        {
+            Console.WriteLine("Olá Mudo!");
+        }
     }
 }
